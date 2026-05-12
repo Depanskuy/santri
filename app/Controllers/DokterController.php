ba0a0eacdce3a4c935eecdff05752ad807825dd4
Kalri <53751770+KhairiFadhil@users.noreply.github.com>
2026-05-12 10:36:02 +0700

    ganti database implementation di contropller dari dummy ke mysql

--- app/Controllers/DokterController.php
+++ app/Controllers/DokterController.php
@@ -2,20 +2,13 @@
     namespace App\Controllers; 
     
     use App\Core\View;
-
+    use App\Model\Dokter;
+       
     class DokterController
     {
         public function detail($id)
         {
-            $data = require __DIR__ . "/../../data/dummy.php";
-
-            $dokter = null;
-            foreach ($data['doctors'] as $d){
-                if ($d['id'] == $id){
-                    $dokter = $d;
-                }
-            }
-
+            $dokter = Dokter::findById((int)$id);
             if(!$dokter){
                 http_response_code(400);
                 echo "<h1> Dokter tidak ada </h1>";
